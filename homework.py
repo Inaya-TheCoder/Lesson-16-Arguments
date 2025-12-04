7212ec8f630098950bdc3ed250da69d854fbb611
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-12-04 20:51:17 +0530

    homework.py

---
+++ homework.py
@@ -0,0 +1,7 @@
+a = input("Do u want to shut the program down? :")
+if a == "yes":
+    print ("Shutting Down")
+if a == "no":
+    print ("Abort Shutting Down")
+else:
+    print("Sorry,I cant understand what you want.")
